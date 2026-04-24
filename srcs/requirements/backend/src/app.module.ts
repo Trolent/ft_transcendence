458a7c7cbb9e418ec9d341c1841c5a0947c11833
fripokoff <azceus@gmail.com>
2026-04-24 18:06:18 +0200

    feat(prisma): add schema, migrations, PrismaService and PrismaModule

--- srcs/requirements/backend/src/app.module.ts
+++ srcs/requirements/backend/src/app.module.ts
@@ -1,5 +1,9 @@
+import { PrismaModule } from './prisma/prisma.module';
 import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 
-@Module({ controllers: [AppController] })
+@Module({
+    controllers: [AppController],
+    imports: [PrismaModule]
+})
 export class AppModule {}
