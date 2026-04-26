d55648cd8e206cf52a03372d3a9367a9bfdc7624
Jerome Berg <77480853+jeromeberg@users.noreply.github.com>
2026-04-27 00:01:16 +0200

    fix: breakpoint sidebar

--- srcs/requirements/frontend/src/layout/PageWithSidebar.tsx
+++ srcs/requirements/frontend/src/layout/PageWithSidebar.tsx
@@ -7,11 +7,11 @@ interface PageWithSidebarProps {
 
 export default function PageWithSidebar({ children, sidebar }: PageWithSidebarProps) {
   return (
-    <div className="flex flex-col lg:flex-row gap-6 p-6">
-      <aside className="w-full lg:w-64 shrink-0 flex flex-col gap-6 order-first lg:order-last">
+    <div className="flex flex-col sm:flex-row gap-6 p-6">
+      <aside className="w-full sm:w-64 shrink-0 flex flex-col gap-6 order-first sm:order-last">
         {sidebar}
       </aside>
-      <div className="flex-1 flex flex-col gap-6 order-last lg:order-first">
+      <div className="flex-1 flex flex-col gap-6 order-last sm:order-first">
         {children}
       </div>
     </div>
