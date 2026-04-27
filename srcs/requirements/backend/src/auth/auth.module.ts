07357932bee088f6fa82b22ac29c7ad966117d78
fripokoff <azceus@gmail.com>
2026-04-27 14:22:20 +0200

    fix(infra): use named volumes for node_modules persistence

--- srcs/requirements/backend/src/auth/auth.module.ts
+++ srcs/requirements/backend/src/auth/auth.module.ts
@@ -3,7 +3,7 @@ import { JwtModule } from '@nestjs/jwt';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { JwtStrategy } from './jwt.strategy';
-import { UsersModule } from '../users/users.modules';
+import { UsersModule } from '../users/users.module';
 
 @Module({
   imports: [
