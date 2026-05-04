595514285127698e723a022023e27eff77e27386
axbaudri <axbaudri@Typerun.typerun.fr>
2026-05-04 18:14:18 +0200

    updated setting menu disposition

--- srcs/requirements/frontend/src/pages/Settings.tsx
+++ srcs/requirements/frontend/src/pages/Settings.tsx
@@ -1,28 +1,31 @@
-import { Link } from "react-router-dom";
-
-import { Avatar, Heading, List, Text } from "../components";
+import { Heading, Text } from "../components";
+import Container from "../components/Container";
 import { PageLayout } from "../layout";
 
-const options: { id: number; name: string }[] = [
-  { id: 1, name: "Edit bio" },
-  { id: 2, name: "Game mode" },
-  { id: 3, name: "Language" },
-];
-
-export default function Leaderboard() {
+export default function Settings() {
   return (
     <PageLayout maxWidth="max-w-lg">
       <Heading level={2}>SETTINGS</Heading>
-      <List
-        className="mt-6"
-        containerVariant="panel"
-        items={options}
-        renderItem={(item) => (
-        <div className="flex items-center gap-4 transition-opacity hover:opacity-80 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-default">
-          <Text>{item.name}</Text>
+        <div className="mt-3 flex-col hover:opacity-80">
+          <Container variant="panel">
+            <Text>Edit bio</Text>
+            <div class="edit-bio">
+              <Container variant="default">
+              Bonjour
+            </Container>
+            </div>
+          </Container>
         </div>
-        )}
-      />
+        <div className="mt-3 flex-col hover:opacity-80">
+          <Container variant="panel">
+            <Text>Game mode</Text>
+          </Container>
+        </div>
+        <div className="mt-3 flex-col hover:opacity-80">
+          <Container variant="panel">
+            <Text>Language</Text>
+          </Container>
+        </div>
     </PageLayout>
   );
 }
