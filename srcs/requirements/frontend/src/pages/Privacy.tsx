7ece91eef9d88e8c9fff63ce777042d4baadfbd1
Axel Baudrillart <axbaudri@f2r3s15.clusters.42paris.fr>
2026-05-20 17:01:12 +0200

    responsive footer

--- srcs/requirements/frontend/src/pages/Privacy.tsx
+++ srcs/requirements/frontend/src/pages/Privacy.tsx
@@ -6,6 +6,7 @@ export default function Privacy() {
     <PageLayout>
       <Heading level={2}>Privacy</Heading>
       <Text variant="dim"><u>Vos données:</u> Lorsque vous créez un compte, les données collectées sont les suivants : nom d'utilisateur, adresse e-mail et mot de passe.</Text>
+      <Text variant="dim"><u>Utilisation des données:</u> Nous nous engageons à garder une totale transparence quand aux données recueillies.</Text>
     </PageLayout>
   );
 }
